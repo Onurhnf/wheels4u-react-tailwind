2b852e985de2ec8856af4526ce406fe93bac9fe3
Onur Eren Hanife <onurhnf@gmail.com>
2023-07-26 01:11:29 +0300

    table data

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,20 +3,15 @@ import Table from "../components/ui/Table.jsx";
 
 function Dashboard() {
   const bookings = [
-    // CABIN 001
     {
-      created_at: "fromToday(-20, true)",
-      startDate: "fromToday(0)",
+      created_at: "asd",
+      startDate: "asd",
       endDate: "asd",
-      cabinId: 1,
-      guestId: 2,
-      hasBreakfast: true,
-      observations:
-        "I have a gluten allergy and would like to request a gluten-free breakfast.",
       isPaid: false,
       numGuests: 1,
     },
   ];
+
   return (
     <>
       <Table columns="[0.6fr_2fr_2.4fr_1.4fr_1fr_3.2rem]">
